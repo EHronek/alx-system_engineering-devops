8480f99b1da2158bdcc88df832d62f2deff48e04
EHronek <emmanuelhronek@gmail.com>
2025-02-11 23:42:39 +0300

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,35 +5,30 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
     emp_id = sys.argv[1]
     user_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
     todos_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
 
-    try:
-        user_response = requests.get(user_url)
-        user_response.raise_for_status()
-        emp_data = user_response.json()
-        username = emp_data.get("username")
+    
+    user_response = requests.get(user_url)
+    user_response.raise_for_status()
+    emp_data = user_response.json()
+    username = emp_data.get("username")
 
-        todos_response = requests.get(todos_url)
-        todos_response.raise_for_status()
-        todos_json_data = todos_response.json()
+    todos_response = requests.get(todos_url)
+    todos_response.raise_for_status()
+    todos_json_data = todos_response.json()
 
-        tasks = {str(emp_id): []}
-        for task in todos_json_data:
-            TASK_COMPLETED_STATUS = task.get('completed')
-            TASK_TITLE = task.get('title')
-            tasks[emp_id].append({
-                                  "task": TASK_TITLE,
-                                  "completed": TASK_COMPLETED_STATUS,
-                                  "username": username})
+    tasks = {str(emp_id): []}
+    for task in todos_json_data:
+        TASK_COMPLETED_STATUS = task.get('completed')
+        TASK_TITLE = task.get('title')
+        tasks[emp_id].append({
+                              "task": TASK_TITLE,
+                              "completed": TASK_COMPLETED_STATUS,
+                               "username": username})
 
 
-        file_name = f"{emp_id}.json"
-        with open(file_name, 'w') as json_file:
-            json.dump(tasks, json_file, indent=4)
-
-    except requests.exceptions.RequestException as e:
-        sys.exit(1)
+    file_name = f"{emp_id}.json"
+    with open(file_name, 'w') as json_file:
+         json.dump(tasks, json_file, indent=4)
