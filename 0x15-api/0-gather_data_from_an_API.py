307196f7d8b0a1193b4fc78fe37c659da3282d82
EHronek <emmanuelhronek@gmail.com>
2025-02-11 21:15:07 +0300

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Script tghat returns information about an employee using given REST API"""
+import requests
+import sys
+
+
+def get_employee_data(emp_id):
+    """Define the base url for the api"""
+    user_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
+
+    try:
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        employee_data = user_response.json()
+        emp_name = employee_data.get("name")
+
+        todos_response = requests.get(todos_url)
+        todos_response.raise_for_status()
+        todos_data = todos_response.json()
+
+        completed_tasks = [task for task in todos_data if task["completed"]]
+        total_tasks = len(todos_data)
+        completed_tasks_number = len(completed_tasks)
+
+        print(f"Employee {emp_name} is done with tasks({completed_tasks_number}/{total_tasks}):")
+        for task in completed_tasks:
+            print(f"\t {task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    try:
+        emp_id = int(sys.argv[1])
+        get_employee_data(emp_id)
+    except ValueError:
+        sys.exit(1)
