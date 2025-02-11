cf5ed2bc42c941267a5521b8076b5add6ef3b779
EHronek <emmanuelhronek@gmail.com>
2025-02-11 23:55:10 +0300

    task 2

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,6 +3,7 @@
 import json
 import requests
 
+
 if __name__ == "__main__":
     users_url = "https://jsonplaceholder.typicode.com/users"
     todos_url = "https://jsonplaceholder.typicode.com/todos"
