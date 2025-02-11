ad9093f31ca0e8cdc75701dacb678099102a591d
EHronek <emmanuelhronek@gmail.com>
2025-02-11 23:45:34 +0300

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,24 +5,35 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    USER_ID = sys.argv[1]
-    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
-    res = requests.get(url_to_user)
-    """Documentation"""
-    USERNAME = res.json().get('username')
-    """Documentation"""
-    url_to_task = url_to_user + '/todos'
-    res = requests.get(url_to_task)
-    tasks = res.json()
+    if len(sys.argv) != 2:
+        sys.exit(1)
+    emp_id = sys.argv[1]
+    user_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos"
 
-    dict_data = {USER_ID: []}
-    for task in tasks:
-        TASK_COMPLETED_STATUS = task.get('completed')
-        TASK_TITLE = task.get('title')
-        dict_data[USER_ID].append({
-                                  "task": TASK_TITLE,
-                                  "completed": TASK_COMPLETED_STATUS,
-                                  "username": USERNAME})
-    """print(dict_data)"""
-    with open('{}.json'.format(USER_ID), 'w') as f:
-        json.dump(dict_data, f)
+    try:
+        user_response = requests.get(user_url)
+        user_response.raise_for_status()
+        emp_data = user_response.json()
+        username = emp_data.get("username")
+
+        todos_response = requests.get(todos_url)
+        todos_response.raise_for_status()
+        todos_json_data = todos_response.json()
+
+        tasks = []
+        for task in todos_json_data:
+            tasks.append({
+                "task": task["title"],  # TASK_TITLE
+                "completed": task["completed"],  # TASK_COMPLETED_STATUS
+                "username": username  # USERNAME
+            })
+
+        json_data = {str(emp_id): tasks}
+
+        file_name = f"{emp_id}.json"
+        with open(file_name, 'w') as json_file:
+            json.dump(json_data, json_file, indent=4)
+
+    except requests.exceptions.RequestException as e:
+        sys.exit(1)
