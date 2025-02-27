43aa2095fb7daa9dc6ac3da0411270e9eb4ff92b
EHronek <emmanuelhronek@gmail.com>
2025-02-27 23:48:44 +0300

    API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,15 +6,17 @@ import sys
 
 def top_ten(subreddit):
     ''' functionm to return the top ten posts for a given subreddit'''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+    
     try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            print(None)
+    except requests.RequestException:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(sys.argv[1])
