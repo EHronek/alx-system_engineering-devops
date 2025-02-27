db1587a5043ef5a363eee1b5064eeb822ce23ed7
EHronek <emmanuelhronek@gmail.com>
2025-02-27 23:32:28 +0300

    API advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''function top_ten returns top ten posts for a given subreddit'''
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    ''' functionm to return the top ten posts for a given subreddit'''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(sys.argv[1])
