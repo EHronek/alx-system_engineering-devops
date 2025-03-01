96746911a7da1469183bf4f108d3a571914e9fb8
EHronek <emmanuelhronek@gmail.com>
2025-03-01 12:24:18 +0300

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix an error in my file':
   command => 'test -f /var/www/html/wp-settings.php && sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-    path    => '/usr/bin:/bin',
+  path    => '/usr/bin:/bin',
 }
