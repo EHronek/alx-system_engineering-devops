08e70786998becdb5f877bfbcddfec1027517179
EHronek <emmanuelhronek@gmail.com>
2025-03-01 12:01:38 +0300

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixes bug in my wordpress file
 
- exec { 'wordpress_fix':
-   command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
-   path    => '/usr/local/bin:/bin'
+exec { 'wordpress_fix':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-setting.php',
+  path    => '/usr/local/bin:/bin'
 }
